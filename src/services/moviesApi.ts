6f83dd4ee57f477e8a52ba4dbc9bc618e23b8f47
matbelmont <belmontmat@gmail.com>
2025-11-21 11:48:10 -0800

    readded mock getbyid services

--- src/services/moviesApi.ts
+++ src/services/moviesApi.ts
@@ -1,5 +1,6 @@
-import { moviesService } from 'utils/axios';
+import { mockMovieService, moviesService } from 'utils/axios';
 
 export const moviesApi = {
-  search: (config: { params: { q: string } }) => moviesService.get('/api/v1/movies/search', config)
+  search: (config: { params: { q: string } }) => moviesService.get('/api/v1/movies/search', config),
+  getByID: (id: number) => Promise.resolve(mockMovieService.getByID(id))
 };
--- src/services/tvApi.ts
+++ src/services/tvApi.ts
@@ -1,5 +1,6 @@
-import { tvService } from 'utils/axios';
+import { mockTVService, tvService } from 'utils/axios';
 
 export const tvApi = {
-    search: (config: { params: { name: string } }) => tvService.get('/series', config)
+  search: (config: { params: { name: string } }) => tvService.get('/series', config),
+  getByID: (id: number) => Promise.resolve(mockTVService.getByID(id))
 };
