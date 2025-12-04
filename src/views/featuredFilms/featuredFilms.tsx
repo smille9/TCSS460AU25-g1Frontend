3ed6cce3c703bbd77cee6536ff3e49d63d048da9
presia27 <pre.sia977@outlook.com>
2025-12-04 00:48:06 -0800

    Styling for cards using mock data

--- src/views/featuredFilms/featuredFilms.tsx
+++ src/views/featuredFilms/featuredFilms.tsx
@@ -1,16 +1,40 @@
 'use client';
 
-import { Grid2, Stack } from '@mui/material';
-import MovieShowCard from 'components/MovieShowCard';
+import { Box, Stack } from '@mui/material';
+import MovieShowCard from 'components/MovieShowCard/MovieShowCard';
+import { testData } from './temp';
+
+const MOVIE_ROUTE: string = '/movies/'; // /movies/[slug]
 
 export default function FeaturedFilmsView() {
   return (
     <>
       <Stack>
         <h2>Recently Released</h2>
-        <Grid2>
-          <MovieShowCard />
-        </Grid2>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            gap: '8px'
+          }}
+        >
+          <MovieShowCard
+            name="Dune"
+            subtext="Director: Some Guy"
+            imgUrl="https://image.tmdb.org/t/p/w500/d5NXSklXo0qyIYkgV94XAgMIckC.jpg"
+            link={MOVIE_ROUTE + '#'}
+          />
+          {testData.map((entry) => (
+            <MovieShowCard
+              key={entry.movie_id}
+              name={entry.title}
+              subtext={`Director: ${entry.director_name}`}
+              imgUrl={entry.poster_url}
+              link={MOVIE_ROUTE + entry.movie_id}
+            />
+          ))}
+        </Box>
       </Stack>
       <Stack>
         <h2>Top Grossing</h2>
